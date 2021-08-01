caf4f54085aab63e2e2589f0e56ce36e7e10aa43
Markilha <markilha@gmail.com>
2021-07-31 23:16:33 -0300

    Criando Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,30 @@
+import styles from './styles.module.scss';
+import Image from 'next/image';
+import logo from '../../../public/images/logo.svg';
+
+import Link from 'next/link';
+
+export function Header(){
+    return(
+        <header className={styles.headerContainer}>
+            <div className={styles.headerContent}>
+                <a>
+                    <Image src={logo} alt="sujeito Programador"/>
+                </a>
+                <nav>
+                    <Link href="/">
+                        <a>Home</a>
+                    </Link>
+                    <Link href="/posts">
+                        <a>Conteúdos</a>
+                    </Link>
+                    <Link href="/sobre">
+                        <a>Quem somos</a>
+                    </Link>
+                </nav>
+
+                <a className={styles.readyButton} type="button" href="https://sujeitoprogramdor.com">COMEÇAR</a>
+            </div>         
+        </header>        
+    )
+}
