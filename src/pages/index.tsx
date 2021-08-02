592c78dddd14fa30be46bb1465066f62f5e2dccd
Markilha <markilha@gmail.com>
2021-08-01 22:39:55 -0300

    Criando footer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,52 +1,67 @@
 import Head from 'next/head';
 import styles from '../styles/home.module.scss';
+import Image from 'next/image';
+import techsImage from '../../public/images/techs.svg';
 
 export default function Home() {
   return (
-   <>
-    <Head>
-      <title>Apaixonado por tecnologia - Sujeito Programador</title>
-    </Head>
-    <main className={styles.container}>
-      <div className={styles.containerHeader}>
-        <section className={styles.ctaText}>
-          <h1>Levando você ao próximo nível!</h1>
-          <span>Uma plataforma com cursos que vão do zero até o profissional na pratica, direto ao ponto aplicando o que usamos no mercado de trabalho. 👊</span>
+    <>
+      <Head>
+        <title>Apaixonado por tecnologia - Sujeito Programador</title>
+      </Head>
+      <main className={styles.container}>
+        <div className={styles.containerHeader}>
+          <section className={styles.ctaText}>
+            <h1>Levando você ao próximo nível!</h1>
+            <span>Uma plataforma com cursos que vão do zero até o profissional na pratica, direto ao ponto aplicando o que usamos no mercado de trabalho. 👊</span>
+            <a>
+              <button>
+                COMEÇAR AGORA!
+              </button>
+            </a>
+          </section>
+
+          <img
+            src="/images/banner-conteudos.png"
+            alt="Conteúdos Sujeito Programador"
+          />
+        </div>
+
+        <hr className={styles.divisor} />
+
+        <div className={styles.sectionContent}>
+          <section>
+            <h2>Aprenda criar aplicativos para Android e iOS</h2>
+            <span>Você vai descobrir o jeito mais moderno de desenvolver apps nativos para iOS e Android, construindo aplicativos do zero até aplicativos.</span>
+          </section>
+
+          <img src="/images/financasApp.png" alt="Conteúdos desenvolvimento de apps" />
+        </div>
+
+        <hr className={styles.divisor} />
+
+        <div className={styles.sectionContent}>
+          <img src="/images/webDev.png" alt="Conteúdos desenvolvimento de aplicacoes web" />
+
+          <section>
+            <h2>Aprenda criar sistemas web</h2>
+            <span>Criar sistemas web, sites usando as tecnologias mais modernas e requisitadas pelo mercado.</span>
+          </section>
+        </div>
+
+        <div className={styles.nextLevelContent}>
+          <Image quality={100} src={techsImage} alt="Tecnologias" />
+          <h2>Mais de <span className={styles.alunos}>15 mil</span> já levaram sua carreira ao próximo nivel.</h2>
+          <span>E você vai perder a chance de evoluir de uma vez todas?</span>
           <a>
             <button>
-              COMEÇAR AGORA!
+            Acessar turma
             </button>
           </a>
-        </section>
+        </div>
 
-        <img 
-            src="/images/banner-conteudos.png" 
-            alt="Conteúdos Sujeito Programador" 
-        />
-      </div>
 
-      <hr className={styles.divisor} />
-
-      <div className={styles.sectionContent}>
-        <section>
-          <h2>Aprenda criar aplicativos para Android e iOS</h2>
-          <span>Você vai descobrir o jeito mais moderno de desenvolver apps nativos para iOS e Android, construindo aplicativos do zero até aplicativos.</span>
-        </section>
-
-        <img src="/images/financasApp.png" alt="Conteúdos desenvolvimento de apps" />
-      </div>
-
-      <hr className={styles.divisor} />
-
-      <div className={styles.sectionContent}>
-        <img src="/images/webDev.png" alt="Conteúdos desenvolvimento de aplicacoes web" />
-
-        <section>
-          <h2>Aprenda criar sistemas web</h2>
-          <span>Criar sistemas web, sites usando as tecnologias mais modernas e requisitadas pelo mercado.</span>
-        </section>
-      </div> 
-    </main>
-   </>
+      </main>
+    </>
   )
 }
