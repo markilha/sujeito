58fa8b04037bc03e8d6ce01e461c48f0bebd0463
Markilha <markilha@gmail.com>
2021-08-02 00:18:36 -0300

    Usando um CMS

---
+++ src/services/prismic.ts
@@ -0,0 +1,11 @@
+import Prismic from '@prismicio/client';
+
+
+export function getPrismicClient(req?: unknown){
+    const prismic = Prismic.client('https://markilha-sujeito.cdn.prismic.io/api/v2',{
+        req,
+    })
+
+    return prismic;
+
+}
