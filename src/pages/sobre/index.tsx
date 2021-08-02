5981b3174f062fb575944701a22c2e413f8504f2
Markilha <markilha@gmail.com>
2021-08-02 14:50:59 -0300

    Finalizando projeto

--- src/pages/sobre/index.tsx
+++ src/pages/sobre/index.tsx
@@ -86,7 +86,8 @@ export const getStaticProps: GetStaticProps = async () => {
   return{
     props:{
       content
-    }
+    },
+    revalidate: 60 * 15 // A cada 15 minutos vai ser revalidade
   }
 }
 
