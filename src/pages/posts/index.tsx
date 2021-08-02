267f15e153bca1f5032a7bbb775b0b638c5f2611
Markilha <markilha@gmail.com>
2021-08-02 10:46:52 -0300

    Consumindo posts pt2

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,35 +1,37 @@
 import { GetStaticProps } from 'next';
+import { useState } from 'react';
+
 import Head from 'next/head';
 
 import styles from './styles.module.scss';
 import Link from 'next/link';
 
 import Image from 'next/image';
-import thumbImg from '../../../public/images/thumb.png';
+
+import { getPrismicClient } from '../../services/prismic';
+import Prismic from '@prismicio/client';
+import { RichText} from 'prismic-dom'; 
+
+// https://png-pixel.com/
 
 import { FiChevronLeft, FiChevronsLeft, FiChevronRight, FiChevronsRight} from 'react-icons/fi';
-
-import {getPrismicClient} from '../../services/prismic';
-import Prismic from '@prismicio/client';
-import {RichText} from 'prismic-dom';
 
 type Post = {
   slug: string;
   title: string;
   cover: string;
   description: string;
-  updatedAt: string;
-
+  updatedAt: string
 }
 
 interface PostsProps{
   posts: Post[];
 }
 
+export default function Posts({ posts: postsBlog }: PostsProps){
 
-export default function Posts({posts}: PostsProps){
+  const [posts, setPosts] = useState(postsBlog || []);
 
-  console.log(posts);
   return(
     <>
      <Head>
@@ -37,20 +39,24 @@ export default function Posts({posts}: PostsProps){
      </Head>
      <main className={styles.container}>
        <div className={styles.posts}>
-         <Link href="/">
-          <a>
-            <Image 
-              src={thumbImg} 
-              alt="Post titulo 1"
-              width={720}
-              height={410}
-              quality={100}
-            />
-            <strong>Criando meu primeiro aplicativo</strong>
-            <time>14 JULHO 2021</time>
-            <p>Hoje vamos criar o controle de mostrar a senha no input, uma opção para os nossos formulários de cadastro e login. Mas chega de conversa e bora pro código junto comigo que o vídeo está show de bola!</p>
-          </a>
-         </Link>
+        {posts.map( post => (
+          <Link key={post.slug} href={`/posts/${post.slug}`}>
+            <a key={post.slug}>
+              <Image 
+                src={post.cover} 
+                alt={post.title}
+                width={720}
+                height={410}
+                quality={100}
+                placeholder="blur"
+                blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mN0vQgAAWEBGHsgcxcAAAAASUVORK5CYII="
+              />
+              <strong>{post.title}</strong>
+              <time>{post.updatedAt}</time>
+              <p>{post.description}</p>
+            </a>
+          </Link>
+        ))}
 
          <div className={styles.buttonNavigate}>
             <div>
@@ -78,37 +84,38 @@ export default function Posts({posts}: PostsProps){
   )
 }
 
-export const getStaticProps: GetStaticProps = async ()=>{
 
-  const prismc = getPrismicClient();
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
 
-  const response = await prismc.query([
-    Prismic.Predicates.at('document.type','post')
-  ],{
-    orderings:'[document.last_publication_date desc]',//Ordenar pelo mais recente
-    fetch: ['post.title','post.description','post.cover'],
+  const response = await prismic.query([
+    Prismic.Predicates.at('document.type', 'post')
+  ], {
+    orderings: '[document.last_publication_date desc]', //Ordenar pelo mais recente
+    fetch: ['post.title', 'post.description', 'post.cover'],
     pageSize: 3
   })
 
-  const posts = response.results.map(post=>{
-    return{
+  // console.log(JSON.stringify(response, null, 2))
+
+  const posts = response.results.map( post => {
+    return {
       slug: post.uid,
       title: RichText.asText(post.data.title),
       description: post.data.description.find(content => content.type === 'paragraph')?.text ?? '',
       cover: post.data.cover.url,
-      updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR',{
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
         day: '2-digit',
         month: 'long',
         year: 'numeric'
       })
-     }
+    }
   })
 
- 
   return{
     props:{
       posts
     },
-    revalidate: 60 * 30 // atualiza a cada 30 minutos
+    revalidate: 60 * 30 // Atualiza a cada 30 minutos.
   }
 }
